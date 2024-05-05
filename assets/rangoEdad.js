8af50215cd5abd3e3a254aed170af3ad40e5b654
Gayoso Facundo <facusbrr@gmail.com>
2024-05-05 15:39:25 -0300

    Se termino rangoEdad

--- assets/rangoEdad.js
+++ assets/rangoEdad.js
@@ -1,10 +1,13 @@
+let pedirEdad = Number(prompt ("Escriba una edad: "));
 
-
-switch (key) {
-    case value:
-        
-        break;
-
-    default:
-        break;
+if (pedirEdad >= 0 && pedirEdad <= 12){
+    console.log ("Se encuentra en un rango de edad de un niño");
+} else if (pedirEdad >= 13 && pedirEdad <= 19) {
+    console.log ("Se encuentra en una edad adolescente");
+} else if (pedirEdad >= 20 && pedirEdad <= 59) {
+    console.log ("Se encuentra en una edad adulta");
+} else if (pedirEdad >= 60) {
+    console.log ("Se encuentra en una edad de adulto mayor");
+} else {
+    console.log ("Ingrese un número válido");
 }
