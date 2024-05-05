aa550dd1f58619ab5f47e1f9595fa681be4b76be
Gayoso Facundo <facusbrr@gmail.com>
2024-05-05 16:15:13 -0300

    Se termino el último enunciado de dia de la Semana

---
+++ assets/js/diaSemana.js
@@ -0,0 +1,35 @@
+let numero = Number (prompt("Ingrese un número del 1 al 7: "));
+
+switch (numero) {
+    case 1:
+        console.log ("El día es Lunes")
+    break;
+
+    case 2:
+        console.log ("El día es Martes")
+    break;
+
+    case 3:
+        console.log ("El día es Miércoles")
+    break;
+
+    case 4:
+        console.log ("El día es Jueves")
+    break;
+
+    case 5:
+        console.log ("El día es Viernes")
+    break;
+
+    case 6:
+        console.log ("El día es Sábado")
+    break;
+
+    case 7:
+        console.log ("El día es Domingo")
+    break;
+
+    default:
+        console.log ("Ingresar un número válido")
+    break;
+}
